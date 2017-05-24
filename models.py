8afb3e24f18e513aec41f7da5646ae0f1fdddd63
S. Dossett Swift <sdossettswift@gmail.com>
2017-05-23 23:42:01 -0500

    updated gitignore, added Item model, registered Item with admin site

--- models.py
+++ models.py
@@ -4,4 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+
+class Item(models.Model):
+    user = models.ForeignKey('auth.User')
+    item_name = models.CharField(max_length=50)
+    completed = models.BooleanField(default=False)
+    date_added = models.DateTimeField(
+            default=timezone.now)
+    date_completed = models.DateTimeField(
+            blank=True, null=True)
+
+    def complete(self):
+        self.date_completed = timezone.now()
+        self.completed = True
+        self.save()
+
+    def __str__(self):
+        return self.item_name
